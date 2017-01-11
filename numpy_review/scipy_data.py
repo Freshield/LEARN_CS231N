2bbe2e3d8505b66f0dc5e65fe5a84f4d7da8aafb
Freshield <zxdsw199182@gmail.com>
2017-01-11 16:08:23 -0500

    done scipy

--- numpy_review/scipy_data.py
+++ numpy_review/scipy_data.py
@@ -8,3 +8,12 @@ img_tinted = imresize(img_tinted, (300,300))
 
 imsave('cat_tinted.jpg', img_tinted)
 
+from scipy.spatial.distance import pdist, squareform
+import numpy as np
+
+x = np.array([[0,1],[1,0],[2,0]])
+print x
+d = squareform(pdist(x, 'euclidean'))
+print d
+print
+
