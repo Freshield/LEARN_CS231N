807524875882b30385d5eafb79443948775657b0
Freshield <zxdsw199182@gmail.com>
2017-01-14 15:55:49 -0500

    done numpy

--- numpy_review/numpy_data.py
+++ numpy_review/numpy_data.py
@@ -152,3 +152,36 @@ print v
 print v.T
 print
 
+x = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+v = np.array([1,0,1])
+y = np.empty_like(x)
+print y
+
+for i in range(4):
+    y[i, :] = x[i, :] + v
+
+print y
+print
+
+vv = np.tile(v, (4,1))
+print vv
+
+y = x + vv
+print y
+
+y = x + v
+print y
+
+v = np.array([1,2,3])
+w = np.array([4,5])
+print np.reshape(v, (3,1)) * w
+print np.reshape(v, (3,1))
+print x
+print np.reshape(x, (6,2))
+print
+
+x = np.array([[1,2,3],[4,5,6]])
+print x + v
+print (x.T + w).T
+print x + np.reshape(w, (2,1))
+print x * 2
