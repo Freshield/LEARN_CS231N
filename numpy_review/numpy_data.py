fb874eccb856bbe12b57fbf0716a0397c316bd97
Freshield <zxdsw199182@gmail.com>
2017-01-15 15:45:01 -0500

    done np matrix

--- numpy_review/numpy_data.py
+++ numpy_review/numpy_data.py
@@ -99,3 +99,56 @@ x = np.array([1,2], dtype=np.int64)
 print x.dtype
 print
 
+x = np.array([[1,2],[3,4]], dtype=np.float64)
+y = np.array([[5,6],[7,8]], dtype=np.float64)
+
+print x + y
+print np.add(x,y)
+
+print x - y
+print np.subtract(x,y)
+
+print x * y
+print np.multiply(x,y)
+
+print x / y
+print np.divide(x,y)
+
+print np.sqrt(x)
+print
+
+v = np.array([9,10])
+w = np.array([11,12])
+
+print v.dot(w)
+print np.dot(v,w)
+print np.dot(w,v)
+print
+
+print x.dot(v)
+print np.dot(x, v)
+print np.dot(v, x)
+
+print x.dot(y)
+print np.dot(x,y)
+print
+
+x = np.array([[1,2],[3,4]])
+
+print np.sum(x)
+print np.sum(x, axis=0)
+print np.sum(x, axis=1)
+print
+
+print x
+print x.T
+print
+
+v = np.array([1,2,3])
+print v
+print v.T
+v = np.array([[1,2,3]])
+print v
+print v.T
+print
+
