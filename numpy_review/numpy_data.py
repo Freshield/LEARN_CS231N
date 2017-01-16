a708219d7376c2cfcd7ec0ac9d79c918fc51553e
Freshield <zxdsw199182@gmail.com>
2017-01-16 15:33:05 -0500

    done dtype

--- numpy_review/numpy_data.py
+++ numpy_review/numpy_data.py
@@ -89,3 +89,13 @@ print a[bool_idx]
 print a[a > 2]
 print
 
+x = np.array([1,2])
+print x.dtype
+
+x = np.array([1.0,2.0])
+print x.dtype
+
+x = np.array([1,2], dtype=np.int64)
+print x.dtype
+print
+
