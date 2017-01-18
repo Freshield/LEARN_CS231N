13606a4d885ef07db0b4e4737cda9f51d59fc437
Freshield <zxdsw199182@gmail.com>
2017-01-18 15:16:29 -0500

    done access

--- numpy_review/numpy_data.py
+++ numpy_review/numpy_data.py
@@ -36,3 +36,49 @@ e = np.random.random((2,2))
 print e
 print
 
+a = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+print a
+b = a[:2, 1:3]
+print b
+print a[0,1]
+b[0,0] = 77
+print a[0,1]
+
+row_r1 = a[1, :]
+row_r2 = a[1:2, :]
+print row_r1, row_r1.shape
+print row_r2, row_r2.shape
+print
+row_r1[0] = 99
+print a
+print row_r1
+print row_r2
+row_r1[0] = 5
+a[0,1] = 2
+print a.shape
+print
+
+col_r1 = a[:, 1]
+col_r2 = a[:, 1:2]
+print col_r1, col_r1.shape
+print col_r2, col_r2.shape
+print
+
+a1 = np.array([[1,2], [3,4], [5,6]])
+print a1[[0,1,2],[0,1,0]]
+print np.array([a1[0,0], a1[1,1], a1[2,0]])
+print
+
+print a[[0,0],[1,1]]
+print np.array([a[0,1], a[0,1]])
+print
+
+print a
+a = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+print a
+b = np.array([0,2,0,1])
+print a[np.arange(4), b]
+a[np.arange(4), b] += 10
+print a
+print
+
