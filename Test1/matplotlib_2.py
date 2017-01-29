04263a77ea85b340994faafd30933578fe3c660f
Freshield <zxdsw199182@gmail.com>
2017-01-29 17:05:19 -0500

    done numpy tutorial

---
+++ Test1/matplotlib_2.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy.misc import imread, imresize
+import matplotlib.pyplot as plt
+
+img = imread('cat.jpg')
+img_tinted2 = img * [1,0.95,0.9]
+
+plt.subplot(1,2,1)
+plt.imshow(img)
+
+plt.subplot(1,2,2)
+plt.imshow(np.uint8(img_tinted2))
+
+plt.show()
