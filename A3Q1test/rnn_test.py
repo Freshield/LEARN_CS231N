fa2e942134d7333ed03a940fc8099d9618faaa0d
freshield <zxdsw199182@gmail.com>
2017-03-22 14:05:28 -0400

    done the word embedding backward

--- A3Q1test/rnn_test.py
+++ A3Q1test/rnn_test.py
@@ -18,16 +18,38 @@ print d
 
 N, T, V, D = 2, 4, 5, 2
 
-x = np.asarray([[0, 3, 1, 2], [0, 1, 2, 3]])
+x = np.asarray([[1, 2], [0, 1]])
 W = np.array([
-    ['I','','',''],
-    ['L','O','V','E'],
-    ['Y','O','U',''],
-    ['!','','','']
+    [0,1,2],
+    [3,4,5],
+    [6,7,8]
 ])
+out = W[x,:]
 print 'x'
 print x
 print 'W'
 print W
-print 'W[x,:]'
-print W[x,:]
+print 'out'
+print out
+print 'dW'
+dW = np.zeros_like(W)
+np.add.at(dW, x, W[x,:])
+print dW
+
+dout = [[[0.1,0.0,0.2],[0.3,0.1,0.0]],[[0.2,0.1,0.1],[0.1,0.2,0.0]]]
+dW2 = np.zeros_like(W, dtype=np.float64)
+for index,idx in enumerate(x):
+
+    dW2[idx] += dout[index]
+
+print 'dW2',dW2
+
+print 'a'
+
+
+a = np.array([[1,2,3,4],[5,6,7,8]])
+b = np.array([5,6])
+np.add.at(a,[[0,1],[1,2]],b)
+print a
+
+print
